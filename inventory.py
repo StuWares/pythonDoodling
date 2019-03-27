dc551479d3f7b14ce78ba18acaa8c0a45effdc8c
Stu Wares <stuwares@outlook.com>
2019-03-27 22:15:49 +0000

    Adding the inventory script

---
+++ inventory.py
@@ -0,0 +1,27 @@
+pockets = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory):
+    print('Inventory:')
+
+    item_total = 0
+    for item, quantity in inventory.items():
+        print(str(quantity) + ' - ' + item)
+        item_total += quantity
+
+    print("Total items collected: " + str(item_total))
+
+
+displayInventory(pockets)
+
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return inventory
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
